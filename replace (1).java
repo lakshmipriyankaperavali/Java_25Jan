0630417b182912852ad750aae8716b0777def2df
lakshmipriyankaperavali <59957996+lakshmipriyankaperavali@users.noreply.github.com>
2020-02-01 11:41:12 +0530

    Add files via upload

---
+++ replace (1).java
@@ -0,0 +1,9 @@
+class replace
+{
+	public static void main(String args[])
+	{
+		String s1="java is a programming language.java is a technology";
+		String replaceString=s1.replace("java","python");
+		System.out.println(replaceString);
+	}
+}
